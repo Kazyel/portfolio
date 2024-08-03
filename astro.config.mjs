ee6503906de51fc2998336eca07e31ec58dd3596
Kazyel <m.mascarelo@gmail.com>
2024-08-02 23:17:11 -0300

    Base URL change

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://kazyel.github.io",
-  base: "/",
+  base: "/portfolio",
   integrations: [react(), tailwind()]
 });
