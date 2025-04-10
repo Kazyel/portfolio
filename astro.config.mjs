8932b09967ed40eb7f3a4e8aeb53d9ad660ba1bc
Kazyel <m.mascarelo@gmail.com>
2025-04-10 19:59:40 -0300

    vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
 
+import react from "@astrojs/react";
+import vercel from '@astrojs/vercel';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://kazyel.github.io",
+  output: 'server',
+  adapter: vercel(),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
