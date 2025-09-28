000c449d6b13ee0693f57f54cf1eff6929c27a13
kazyel <m.mascarelo@gmail.com>
2025-09-27 22:58:21 -0300

    new eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,15 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript"],
+
+    rules: {
+      'react/no-unescaped-entities': 'off',
+      'no-unused-vars': 'warn',
+      'react-hooks/exhaustive-deps': 'warn',
+    }
+  }),
 ];
 
 export default eslintConfig;
